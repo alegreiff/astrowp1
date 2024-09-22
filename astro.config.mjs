6ddb2db7904473f16b86d755a0fcc7e1bb6b7e0c
Jaime de Greiff <alegreiff@gmail.com>
2024-09-22 16:09:15 -0500

    environmental fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ['wordpress.alegreiff.com']
+    
+  },
   integrations: [tailwind({
     applyBaseStyles: false,
-  })]
+  }), react()],
+  
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}' , 
 		"./node_modules/@wp-block-tools/styles/**/*.js"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"event-horizon": "var(--color--event-horizon)",
+			}
+		},
 	},
 	plugins: [],
 }
